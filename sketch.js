438c464566508722210e32de37c44c9a691e21de
Satyaki <satyakisarkar7500@gmail.com>
2020-09-09 16:54:40 +0530

    C31

--- sketch.js
+++ sketch.js
@@ -79,6 +79,8 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
+        Matter.Body.setPosition(bird.body,{x : 200, y : 50});
         slingshot.attach(bird.body);
+        bird.trajectory = [];
     }
 }
